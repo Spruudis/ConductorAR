8ca58f102ac388922f8fb8a8a464bc9a0fc707b4
James Kim <777jameskim@gmail.com>
2020-06-16 19:57:01 +0900

    Added countdown animation

--- Assets/Animations/AnimationScript.cs
+++ Assets/Animations/AnimationScript.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class AnimationScript : MonoBehaviour
 {
     Animator animator;
-    int clock=0;
     int framerate=64;
-    int counter=0;
-    int wavewait=10;
+    float idletime=0;
 	// Start is called before the first frame update
     void Start()
     {
@@ -17,21 +15,18 @@ public class AnimationScript : MonoBehaviour
 
     void triggerJump(){
         animator.SetTrigger("Jump");
-        counter=0;
+        idletime=0;
     }
     void triggerWave(){
         animator.SetTrigger("Wave");
-        counter=0;
+        idletime=0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        animator.SetInteger("Clock",clock);
-        animator.SetInteger("Counter",counter);
-        clock++;
-        clock%=framerate;
-        if(clock==0){counter++;}
+        idletime+=Time.deltaTime;
+        animator.SetFloat("Clock",idletime);
         if(Input.GetKeyDown(KeyCode.Space)){
             triggerJump();
         }
